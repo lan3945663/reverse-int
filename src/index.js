d4acef73996120cba4bd8cf884a8064d5b7a8dca
lan3945663 <lan3945663@gmail.com>
2021-02-16 21:24:18 +0300

    feat: add reverse number function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+  let str;
+  let reversedStr = '';
+
+  if(n < 0) {
+    str = String(-1 * n);
+  } else {
+    str = String(n);
+  } 
   
+  for(let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+
+  const res = Number(reversedStr)
+  return res
 }
